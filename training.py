6f22e5186dcdc3155566ec617d600dbc115bfd66
Mateusz Bielski <mateo.bass@interia.pl>
2020-01-24 13:41:25 +0100

    removing letter from gradient works correct

--- training.py
+++ training.py
@@ -7,6 +7,7 @@ from tensorflow import keras
 import numpy as np
 train_images = np.load('letters_60000_28x28.npy')
 train_labels = np.load('gray_60000_28x28.npy')
+train_labels = train_labels.reshape(60000,28*28)/255
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
     keras.layers.Dense(128, activation='relu'),
@@ -16,4 +17,3 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 model.fit(train_images, train_labels, epochs=10)
-
