f84ebf56bbe64e0f7331cf12f3383dca6713a5e9
Mateusz Bielski <mateo.bass@interia.pl>
2020-02-07 12:49:56 +0100

    removing circles not working despite despite using CNN

---
+++ GradientLetters_DiffCircles.py
@@ -0,0 +1,36 @@
+import time as tm
+import numpy as np
+import Gradient as gd
+import generowanieWieluGradientow as gwg
+
+if __name__ == "__main__":
+    #~ DataParameters
+    num_train = 6000
+    size = 32
+    num_test = None
+    numToShow = 20
+    #~ Generate
+    start = tm.time()
+    print('generowanie danych...')
+    commonFunctions = [gwg.CreateGrayGradientsAsNdArray((num_train,size,size)),gwg.CreateLettersOnGradients]
+    diffFunctions = [gwg.CreateRandomCirclesOn]
+    
+    train_labels = gwg.GenerateDataUsingFunctions(commonFunctions)
+    train_images = gwg.ModifyDataUsingFunctions(train_labels,diffFunctions)
+    stop = tm.time()
+    print('wygenerowano dane w '+'%.5f'%(stop - start)+'s')
+    #~ Save
+    start = stop
+    nameCommon = 'GradientLetters_'
+    nameDiff = nameCommon+'_DiffCircles_'+str(num_train)+'_'+str(size)+'x'+str(size)
+    nameCommon = nameCommon+str(num_train)+'_'+str(size)+'x'+str(size)
+    print(nameCommon)
+    print(nameDiff)
+    
+    np.save(nameCommon,train_labels)
+    np.save(nameDiff,train_images)
+    stop = tm.time()
+    print('zapisano dane w '+'%.5f'%(stop - start)+'s')
+    #~ Show
+    #~ gwg.ShowGeneratedImages(train_images,numToShow)
+    #~ gwg.ShowGeneratedImages(train_labels,numToShow)
